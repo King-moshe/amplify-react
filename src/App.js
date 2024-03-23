687b5884c885a9daf525231893c0cc5b78adef02
Moshe Amar <moso2411@gmail.com>
2024-03-23 22:22:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Amplify test <code>src/App.js</code> try to uploading to Amplify AWS.
         </p>
         <a
           className="App-link"
